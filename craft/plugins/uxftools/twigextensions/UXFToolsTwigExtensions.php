5a05b4cb9068cd5221da46c5b1ded6f2bd10e9ce
Parker Malenke <parker.malenke@pearson.com>
2018-04-02 16:35:26 -0600

    Add colors template etc

--- craft/plugins/uxftools/twigextensions/UXFToolsTwigExtensions.php
+++ craft/plugins/uxftools/twigextensions/UXFToolsTwigExtensions.php
@@ -3,6 +3,7 @@ namespace Craft;
 
 use Twig_Extension;
 use Twig_Filter_Method;
+use Twig_Function_Method;
 
 class UXFToolsTwigExtensions extends \Twig_Extension {
   public function getName() {
@@ -11,6 +12,11 @@ class UXFToolsTwigExtensions extends \Twig_Extension {
   public function getFilters() {
     return array(
       'latestApproved' => new Twig_Filter_Method($this, 'latestApproved'),
+    );
+  }
+  public function getFunctions() {
+    return array(
+      'fourFiveContrasty' => new Twig_Function_Method($this, 'fourFiveContrast')
     );
   }
 
@@ -59,4 +65,51 @@ class UXFToolsTwigExtensions extends \Twig_Extension {
       }
     }
   }
+
+  // calculates the luminosity of an given RGB color
+  // the color code must be in the format of RRGGBB
+  // the luminosity equations are from the WCAG 2 requirements
+  // http://www.w3.org/TR/WCAG20/#relativeluminancedef
+  private function calculateLuminosity($color) {
+    $r = hexdec(substr($color, 0, 2)) / 255; // red value
+    $g = hexdec(substr($color, 2, 2)) / 255; // green value
+    $b = hexdec(substr($color, 4, 2)) / 255; // blue value
+    if ($r <= 0.03928) {
+      $r = $r / 12.92;
+    } else {
+      $r = pow((($r + 0.055) / 1.055), 2.4);
+    }
+    if ($g <= 0.03928) {
+      $g = $g / 12.92;
+    } else {
+      $g = pow((($g + 0.055) / 1.055), 2.4);
+    }
+    if ($b <= 0.03928) {
+      $b = $b / 12.92;
+    } else {
+      $b = pow((($b + 0.055) / 1.055), 2.4);
+    }
+    $luminosity = 0.2126 * $r + 0.7152 * $g + 0.0722 * $b;
+    return $luminosity;
+  }
+
+  // calculates the luminosity ratio of two colors
+  // the luminosity ratio equations are from the WCAG 2 requirements
+  // http://www.w3.org/TR/WCAG20/#contrast-ratiodef
+  private function calculateLuminosityRatio($color1, $color2) {
+    $l1 = $this->calculateLuminosity($color1);
+    $l2 = $this->calculateLuminosity($color2);
+    if ($l1 > $l2) {
+      $ratio = (($l1 + 0.05) / ($l2 + 0.05));
+    } else {
+      $ratio = (($l2 + 0.05) / ($l1 + 0.05));
+    }
+    return $ratio;
+  }
+
+    public function fourFiveContrast($a, $b) {
+      if ($a[0] == '#') $a = substr($a, 1);
+      if ($b[0] == '#') $b = substr($b, 1);
+      return $this->calculateLuminosityRatio($a, $b) >= 4.5;
+    }
 }
