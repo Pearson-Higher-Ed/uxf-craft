c4fbc60e96af555572180226263170ae4eb5cc57
Parker Malenke <parker.malenke@pearson.com>
2018-11-29 10:16:32 -0700

    Fix toc gen module

---
+++ modules/tocgenmodule/src/assetbundles/tocgenmodule/dist/js/TocGenModule.js
@@ -0,0 +1,11 @@
+/**
+ * Toc Gen module for Craft CMS
+ *
+ * Toc Gen JS
+ *
+ * @author    Parker
+ * @copyright Copyright (c) 2018 Parker
+ * @link      google.com
+ * @package   TocGenModule
+ * @since     1.0.0
+ */
---
+++ modules/tocgenmodule/src/translations/en/toc-gen-module.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Toc Gen module for Craft CMS 3.x
+ *
+ * Generator a TOC from html headings
+ *
+ * @link      google.com
+ * @copyright Copyright (c) 2018 Parker
+ */
+
+/**
+ * Toc Gen en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('toc-gen-module', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Parker
+ * @package   TocGenModule
+ * @since     1.0.0
+ */
+return [
+    'Toc Gen plugin loaded' => 'Toc Gen plugin loaded',
+];
